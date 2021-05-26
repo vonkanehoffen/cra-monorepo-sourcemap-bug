152756126d81971c555f61d6313aee706bff0f34
Kane Clover <baron.vonkanehoffen@gmail.com>
2021-05-26 14:32:41 +0100

    load component

--- packages/cra-host/src/App.tsx
+++ packages/cra-host/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Lorem } from "@akj-dev/lorem";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Lorem foo="Hello" />
         <a
           className="App-link"
           href="https://reactjs.org"
